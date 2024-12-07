79f1676b0fc9d762847afd5fc36b825dcb223520
Ibtissem-01 <ibtissem.rahim@outlook.fr>
2024-12-07 17:38:22 +0100

    swagger documentation added

--- app/fr/parisnanterre/cleanup_heroes/backendPython/controller/hello_controller.py
+++ app/fr/parisnanterre/cleanup_heroes/backendPython/controller/hello_controller.py
@@ -1,5 +1,15 @@
 # hello_controller.py
 from django.http import HttpResponse
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from rest_framework.decorators import api_view
 
-def hello_view(request):
-    return HttpResponse("Hello from API", content_type="text/plain")
+
+@swagger_auto_schema(
+    method='get',
+    operation_description="Obtenez un message de salutation",
+    responses={200: openapi.Response(description="Plain text response containing 'Hello API'")}
+)
+@api_view(['GET'])
+def hello_api(request):
+    return HttpResponse("Hello API", content_type="text/plain")
