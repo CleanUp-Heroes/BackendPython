88c0629237719fa370adaf35bd50f62357015716
Ibtissem-01 <ibtissem.rahim@outlook.fr>
2024-12-11 12:08:23 +0100

    configuration of the database in settings.py and add file requirements.txt

--- backendPython/settings.py
+++ backendPython/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'backendPython.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cleanupheroesdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
