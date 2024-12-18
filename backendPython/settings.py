0df8f4bd9519f01b97c4097cab71a0c7d4e73905
Ibtissem-01 <ibtissem.rahim@outlook.fr>
2024-12-18 23:58:17 +0100

    add corsheaders for the request,

--- backendPython/settings.py
+++ backendPython/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# A limiter pour la production
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -40,10 +42,12 @@ INSTALLED_APPS = [
     'app',
     'rest_framework',
     'drf_yasg',
+    'corsheaders'
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
